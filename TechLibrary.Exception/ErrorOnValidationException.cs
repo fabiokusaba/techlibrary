55f25104f234a63870a8584b4738199515322617
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-17 23:02:03 -0300

    created custom exception ErrorOnValidationException

---
+++ TechLibrary.Exception/ErrorOnValidationException.cs
@@ -0,0 +1,21 @@
+using System.Net;
+
+namespace TechLibrary.Exception;
+
+// Exceção customizada
+public class ErrorOnValidationException : TechLibraryException
+{
+    // Somente essa classe vai ter acesso a essa lista de errors
+    // readonly: somente o construtor pode atribuir valores
+    private readonly List<string> _errors;
+    
+    public ErrorOnValidationException(List<string> errorMessages)
+    {
+        _errors = errorMessages;    
+    }
+    
+    public override List<string> GetErrorMessages() => _errors;
+    
+    // Erro 400: a requisição contém problemas
+    public override HttpStatusCode GetStatusCode() => HttpStatusCode.BadRequest;
+}
