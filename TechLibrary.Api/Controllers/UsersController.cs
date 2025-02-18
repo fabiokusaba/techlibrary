ed201e8ba7424ae183f480714b90ad1ecf9ca040
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-17 23:05:01 -0300

    created users controller and implemented create method

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.UseCases.Users.Register;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("v1/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(UserRegisteredResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ErrorMessagesResponse), StatusCodes.Status400BadRequest)]
+    public IActionResult Create(UserRequest request)
+    {
+        // Dentro do try estou tentando executar algo
+        try
+        {
+            // Criando a instância do use case
+            var useCase = new RegisterUserUseCase();
+
+            // Executando a regra de negócio para criação de um usuário
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        // Se alguma exceção ocorrer vou capturá-la no catch
+        catch (TechLibraryException ex)
+        {
+            return BadRequest(new ErrorMessagesResponse
+            {
+                Errors = ex.GetErrorMessages()
+            });
+        }
+        // Qualquer outro tipo de error
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ErrorMessagesResponse
+            {
+                Errors = ["Erro Desconhecido."]
+            });
+        }
+    }
+}
+
