f476af91ef37c0533ef4f6730d01b801a1303153
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-18 19:34:08 -0300

    created BCrypt and HashPassword method

---
+++ TechLibrary.Api/Infraestructure/Security/Cryptography/BCryptAlgorithm.cs
@@ -0,0 +1,6 @@
+namespace TechLibrary.Api.Infraestructure.Security.Cryptography;
+
+public class BCryptAlgorithm
+{
+    public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+}
