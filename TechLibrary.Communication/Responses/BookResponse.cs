40b4f15f923f14a97db414b4e88c2b2cb1f710d3
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-21 20:29:14 -0300

    created book response

---
+++ TechLibrary.Communication/Responses/BookResponse.cs
@@ -0,0 +1,8 @@
+namespace TechLibrary.Communication.Responses;
+
+public class BookResponse
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+}
