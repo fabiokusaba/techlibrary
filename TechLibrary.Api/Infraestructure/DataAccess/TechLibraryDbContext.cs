553aa0edb661e9a8cb1b6bdb3d8012ee2abd414a
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-21 21:57:15 -0300

    added DbSet of Checkouts

--- TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
+++ TechLibrary.Api/Infraestructure/DataAccess/TechLibraryDbContext.cs
@@ -9,6 +9,7 @@ public class TechLibraryDbContext : DbContext
     // Referência para tabelas
     public DbSet<User> Users { get; set; }
     public DbSet<Book> Books { get; set; }
+    public DbSet<Checkout> Checkouts { get; set; }
     
     // Conexão com o banco de dados SQLite
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
