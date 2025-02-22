a217f6af01247c06e3e3b46c7e4d3e16b559e349
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-21 22:00:33 -0300

    created checkouts controller

---
+++ TechLibrary.Api/Controllers/CheckoutsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.Services.LoggedUser;
+using TechLibrary.Api.UseCases.Checkouts;
+
+namespace TechLibrary.Api.Controllers;
+
+[Route("v1/[controller]")]
+[ApiController]
+[Authorize]
+public class CheckoutsController : ControllerBase
+{
+    [HttpPost]
+    [Route("{bookId}")]
+    public IActionResult BookCheckout(Guid bookId)
+    {
+        var loggedUser = new LoggedUserService(HttpContext);
+        
+        var useCase = new RegisterBookCheckoutUseCase(loggedUser);
+        
+        useCase.Execute(bookId);
+        
+        return NoContent();
+    }
+}
+
