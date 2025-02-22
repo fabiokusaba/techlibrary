e3cb65a95e5da3d9d7565da3ea75e7c246abd698
Fabio Kusaba <fabinhokb44@gmail.com>
2025-02-21 21:58:58 -0300

    added jwt configuration

--- TechLibrary.Api/Program.cs
+++ TechLibrary.Api/Program.cs
@@ -1,4 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TechLibrary.Api.Filters;
+
+const string AUTHENTICATION_TYPE = "Bearer";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +15,55 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 // Adicionando documentação do swagger ao projeto
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(AUTHENTICATION_TYPE, new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = AUTHENTICATION_TYPE
+    });
+    
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = AUTHENTICATION_TYPE
+                },
+                Scheme = "oauth2",
+                Name = AUTHENTICATION_TYPE,
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 // Configurando uso de filtros na API
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+// Preparando a API para aceitar tokens JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // Quais opções a API deve considerar
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKey()
+        };
+    });
 
 var app = builder.Build();
 
@@ -33,3 +84,12 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+SymmetricSecurityKey SecurityKey()
+{
+    var signingKey = "BJTCmimxU1cTPtHw3z32Csx4426zuyiS";
+        
+    var symmetricKey = Encoding.UTF8.GetBytes(signingKey);
+        
+    return new SymmetricSecurityKey(symmetricKey);
+}
